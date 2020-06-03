9658b2520ba589d599eaacde7e7f9c6a51ae7f7f
Konstantin Alikhanov <atomAltera@gmail.com>
2020-06-04 00:36:46 +0400

    Adding no-cache flag to docker builds

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build --tag atomaltera/letmeknow-frontend:latest ./frontend/ && \
-docker build --tag atomaltera/letmeknow-backend:latest ./backend/ && \
+docker build --no-cache --tag atomaltera/letmeknow-frontend:latest ./frontend/ && \
+docker build --no-cache --tag atomaltera/letmeknow-backend:latest ./backend/ && \
 docker push atomaltera/letmeknow-frontend:latest && \
 docker push atomaltera/letmeknow-backend:latest
