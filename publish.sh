36846ba5daf39ee9b536ffb3856170bb1c049d9d
Konstantin Alikhanov <atomAltera@gmail.com>
2024-06-18 17:48:13 +0400

    Adjusting publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build --no-cache --tag atomaltera/letmeknow-frontend:latest ./frontend/ && \
-docker build --no-cache --tag atomaltera/letmeknow-backend:latest ./backend/ && \
+docker build --platform linux/amd64 --no-cache --tag atomaltera/letmeknow-frontend:latest ./frontend/ && \
+docker build --platform linux/amd64 --no-cache --tag atomaltera/letmeknow-backend:latest ./backend/ && \
 docker push atomaltera/letmeknow-frontend:latest && \
 docker push atomaltera/letmeknow-backend:latest
